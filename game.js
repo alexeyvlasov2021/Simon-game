156a782ace07ca07d9132eca08e8291d08033916
alexeyvlasov2021 <78164716+alexeyvlasov2021@users.noreply.github.com>
2024-05-31 22:17:38 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -5,132 +5,130 @@ var input = [];
 var clickCounter = 0;
 var animatedCount = 0;
 
-
-
-
-function getRandomNumber(){
-    return Math.floor(Math.random()*4);
+function getRandomNumber() {
+  return Math.floor(Math.random() * 4);
 }
 
-function getColorsArray(){
+function getColorsArray() {
   var output = [];
 
-  for(i = 0; i < maxLevel; i++){
-      output.push(basicColors[getRandomNumber()]);
+  for (i = 0; i < maxLevel; i++) {
+    output.push(basicColors[getRandomNumber()]);
   }
 
   return output;
 }
 
-function animateButton(key){
-    var audio = new Audio("sounds/"+key+".mp3");
-    
-    audio.play();
+function animateButton(key) {
+  var audio = new Audio("sounds/" + key + ".mp3");
 
-    $("#"+key).addClass("pressed");
-    
-    setTimeout(function () {
-        $("#"+key).removeClass('pressed');
-    }, 500);
+  audio.play();
+
+  $("#" + key).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + key).removeClass("pressed");
+  }, 500);
 }
 
-function removeKeypressAnimation(){
-    $(document).off("keypress");
+function removeKeypressAnimation() {
+  $(document).off("keypress");
 }
 
-function removeMouseClick(){
-    $(".btn").off("click");
+function removeMouseClick() {
+  $(".btn").off("click");
 }
 
-function displayCurrentLevel(){
-    setTimeout(function(){
-        $("h1").text("Level"+animatedCount);
-    }, 50);
+function displayCurrentLevel() {
+  setTimeout(function () {
+    $("h1").text("Level: " + animatedCount);
+  }, 850);
 }
 
-function startAnimation(){
-    var key = reference[0];
-    animateButton(key);
-    removeKeypressAnimation();
-    animatedCount++;
-    displayCurrentLevel()
+function startAnimation() {
+  console.log("startAnimation");
+  var key = reference[0];
+  animateButton(key);
+  removeKeypressAnimation();
+  animatedCount++;
+  displayCurrentLevel();
 }
 
-
-
-function getNextAnimation(){
-    setTimeout(function(){
-        animateButton(reference[animatedCount-1]);
-    }, 2000);
-    animatedCount++;
-    displayCurrentLevel()
+function getNextAnimation() {
+  setTimeout(function () {
+    animateButton(reference[animatedCount - 1]);
+  }, 2000);
+  animatedCount++;
+  displayCurrentLevel();
 }
 
+function checkInputValid() {
+  var isValid = true;
 
-function checkInputValid(){
-    var isValid = true;
-    
-    for(var i=0; i <input.length; i++){
-        if(reference[i] != input[i]){
-            isValid = false;
-            break;
-        }
+  for (var i = 0; i < input.length; i++) {
+    if (reference[i] != input[i]) {
+      isValid = false;
+      break;
     }
-    return isValid;
+  }
+  return isValid;
 }
 
-console.log("max level: "+maxLevel);
+console.log("max level: " + maxLevel);
 
-$(document).keypress(startAnimation);
+function playGame() {
+  if (animatedCount > 0) {
+    console.log("animated count is not 0");
 
-$(".btn").click(function(){
+    console.log("inside playGame");
+
     var key = $(this).attr("id");
     console.log(key);
     animateButton(key);
     input.push(key);
     clickCounter++;
-    console.log("click count: "+clickCounter);
-    console.log("animated count: "+animatedCount);
+    console.log("click count: " + clickCounter);
+    console.log("animated count: " + animatedCount);
 
-    if(maxLevel >= clickCounter){ 
-        var isInputValid = checkInputValid();
-        if(isInputValid){
-            console.log("input is valid");
-            console.log("reference: "+reference);
-            console.log("your input: "+input);
-    
-            if((animatedCount == clickCounter) && (clickCounter != maxLevel)){
-                getNextAnimation();
-                input = [];
-                clickCounter = 0;
-            }
+    if (maxLevel >= clickCounter) {
+      var isInputValid = checkInputValid();
+      if (isInputValid) {
+        console.log("input is valid");
+        console.log("reference: " + reference);
+        console.log("your input: " + input);
 
-            if(maxLevel === clickCounter){
-                $("h1").text("WIN");
-                removeMouseClick();
-            }
+        if (animatedCount == clickCounter && clickCounter != maxLevel) {
+          getNextAnimation();
+          input = [];
+          clickCounter = 0;
         }
-        else {
-            $("h1").text("Game over");
 
+        if (maxLevel === clickCounter) {
+          $("h1").text("WIN");
+          removeMouseClick();
+        }
+      } else {
+        $("h1").text("Game over");
 
-            var audio = new Audio("sounds/wrong.mp3");
-    
-            audio.play();
+        var audio = new Audio("sounds/wrong.mp3");
 
-            $("body").addClass("game-over");
-            setTimeout(function(){
-                $("body").removeClass("game-over");
-            }, 250);
-            
-            removeMouseClick();
-        }   
+        audio.play();
+
+        $("body").addClass("game-over");
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 250);
+
+        removeMouseClick();
+      }
     }
-});
+  }
+}
 
+$(document).keypress(startAnimation);
 
+console.log("before IF animatedCount " + animatedCount);
 
-
-
-
-
+// if(animatedCount > 0){
+$(".btn").on("click", playGame);
+// }
